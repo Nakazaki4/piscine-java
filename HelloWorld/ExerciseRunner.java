d8de0ebe5fe1812537b1ba70318d2cf8e6c3f750
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-06 10:17:19 +0100

    null

--- HelloWorld/ExerciseRunner.java
+++ HelloWorld/ExerciseRunner.java
@@ -1,8 +1,6 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        int[] array = ComputeArray.computeArray(new int[]{9, 13, 8, 23, 1, 0, 89});
-        for (int i : array) {
-            System.out.print(i + " ");
-        }
+        System.out.println(StringLength.getStringLength("Hello World !"));
+        System.out.println(StringLength.getStringLength(null));
     }
 }
