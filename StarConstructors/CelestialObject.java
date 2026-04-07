91347fcdbbd6bec069ff8a5d030292f8550db17c
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-07 17:17:14 +0100

    le soleil

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,19 @@
+
+public class CelestialObject {
+    public double x, y, z;
+    public String name;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(double x, double y, double z, String name) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+}
