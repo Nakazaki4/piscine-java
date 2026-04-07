30b694d227001a3b4a656710a65de637479344f1
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-07 15:28:36 +0100

    capitale

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -1,31 +1,28 @@
+
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Capitalize {
+
     public static void capitalize(String[] args) throws IOException {
-        if (args.length < 2)
+        if (args.length != 2) {
             return;
-
-        File inputFile = new File(args[0]);
-        File outputFile = new File(args[1]);
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
-            int c;
-            boolean lastWasSpace = false;
-            while ((c = reader.read()) != -1) {
-                char character = (char) c;
-
-                if (Character.isWhitespace(character)) {
-                    lastWasSpace = true;
-                } else {
-                    if (lastWasSpace) {
-                        writer.write(Character.toUpperCase(character));
-                        lastWasSpace = false;
-                    } else {
-                        writer.write(character);
-                    }
-                }
+        }
+        String filename1 = args[0];
+        String filename2 = args[1];
+        Path path1 = Path.of(filename1);
+        Path path2 = Path.of(filename2);
+        String content = Files.readString(path1);
+        String[] words = content.trim().split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (word.length() > 0) {
+                words[i] = Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
             }
         }
+        String result = String.join(" ", words);
+        Files.writeString(path2, result);
+
     }
 }
