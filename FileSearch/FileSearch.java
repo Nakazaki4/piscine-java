f6097a41eb915d13ed966aad16e731e560d334a4
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-07 16:49:53 +0100

    search

---
+++ FileSearch/FileSearch.java
@@ -0,0 +1,27 @@
+
+import java.io.File;
+
+public class FileSearch {
+    public static String searchFile(String fileName) {
+        File root = new File(fileName);
+        return findFile(root, fileName);
+    }
+
+    private static String findFile(File root, String filename) {
+        File[] files = root.listFiles();
+        if (files == null) {
+            return null;
+        }
+        for (File file : files) {
+            if (file.isDirectory()) {
+                String res = findFile(file, filename);
+                if (res != null) {
+                    return res;
+                }
+            } else if (file.getName().equals(filename)) {
+                return file.getPath();
+            }
+        }
+        return null;
+    }
+}
