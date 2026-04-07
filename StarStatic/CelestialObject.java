2a0bf9f745d6f1221e526cb265be95e6b4e8555c
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-07 18:13:42 +0100

    static

---
+++ StarStatic/CelestialObject.java
@@ -0,0 +1,61 @@
+
+public class CelestialObject {
+    public double x, y, z;
+    public String name;
+    public static double KM_IN_ONE_AU = 150000000.0;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDistanceBetween(CelestialObject obj1, CelestialObject obj2) {
+        return Math.abs(obj1.x - obj2.x);
+    }
+
+    public double getDistanceBetweenInKm(CelestialObject obj1, CelestialObject obj2) {
+        return getDistanceBetween(obj1, obj2) * KM_IN_ONE_AU;
+    }
+
+}
