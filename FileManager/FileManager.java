55a3260a922afe66d28eff702fc0255354178b57
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-07 16:30:14 +0100

    FileManager

---
+++ FileManager/FileManager.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileManager {
+    public static void createFile(String fileName, String content) throws IOException {
+        Files.createFile(Path.of(fileName));
+        Files.writeString(Path.of(fileName), content);
+    }
+
+    public static String getContentFile(String fileName) throws IOException {
+        return Files.readString(Path.of(fileName));
+    }
+
+    public static void deleteFile(String fileName) {
+        try {
+            Files.delete(Path.of(fileName));
+        } catch (Exception e) {
+        }
+    }
+}
