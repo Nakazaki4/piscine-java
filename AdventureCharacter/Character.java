8a8a0857a4031f2d6a9a0f053ca8afbba1bfaddc
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-13 16:44:26 +0100

    win

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,39 @@
+public class Character {
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+
+    public Character(String name, int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth <= 0) {
+            return name + " : KO";
+        }
+        return name + " : " + currentHealth + "/" + maxHealth;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void takeDamage(int damage) {
+        this.currentHealth -= damage;
+    }
+
+    public void attack(Character character) {
+        character.takeDamage(9);
+    }
+}
