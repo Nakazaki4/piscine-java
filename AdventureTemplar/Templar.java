445d7f3467b96d999a0314a8b8169ef1222d67b5
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-13 18:49:46 +0100

    Templar

---
+++ AdventureTemplar/Templar.java
@@ -0,0 +1,35 @@
+public class Templar extends Character implements Healer, Tank {
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int maxHealth, int healCapacity, int shield) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+        this.shield = shield;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    @Override
+    public void heal(Character character) {
+        int newHealth = Math.min(character.getMaxHealth(), character.getCurrentHealth() + this.healCapacity);
+        
+        character.currentHealth = newHealth;
+    }
+
+    @Override
+    public int getShield() {
+        return shield;
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() <= 0) {
+            return this.getName() + " has been beaten, even with its " + this.shield + " shield. So bad, it could heal " + this.healCapacity + " HP.";
+        }
+        return this.getName() + " is a strong Templar with " + this.getCurrentHealth() + " HP. It can heal " + this.healCapacity + " HP and has a shield of " + this.shield + ".";
+    }
+}
