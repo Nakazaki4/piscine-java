4c263c49292b3ca1a4afac94bb710601f0623063
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-14 11:00:21 +0100

    ABSTRACT

---
+++ AdventureAbstract/Sorcerer.java
@@ -0,0 +1,38 @@
+public class Sorcerer extends Character implements Healer {
+    private final int healCapacity;
+
+    public Sorcerer(String name, int maxHealth, int healCapacity) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    @Override
+    public void heal(Character character) {
+        character.setCurrentHealth(character.getCurrentHealth() + healCapacity);
+    }
+
+    @Override
+    public void attack(Character character) {
+        this.heal(this);
+        character.takeDamage(10);
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+         this.setCurrentHealth(damage);
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() <= 0) {
+            return this.getName() + " is a dead sorcerer. So bad, it could heal " + this.healCapacity + " HP.";
+        }
+        return this.getName() + " is a sorcerer with " + this.getCurrentHealth() + " HP. It can heal "
+                + this.healCapacity + " HP.";
+    }
+}
---
+++ AdventureAbstract/Templar.java
@@ -0,0 +1,49 @@
+public class Templar extends Character implements Healer, Tank {
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int maxHealth, int healCapacity, int shield) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+        this.shield = shield;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    @Override
+    public void heal(Character character) {
+        int newHealth = Math.min(character.getMaxHealth(), character.getCurrentHealth() + this.healCapacity);
+
+        character.setCurrentHealth(newHealth);
+    }
+
+    @Override
+    public int getShield() {
+        return shield;
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() <= 0) {
+            return this.getName() + " has been beaten, even with its " + this.shield + " shield. So bad, it could heal "
+                    + this.healCapacity + " HP.";
+        }
+        return this.getName() + " is a strong Templar with " + this.getCurrentHealth() + " HP. It can heal "
+                + this.healCapacity + " HP and has a shield of " + this.shield + ".";
+    }
+
+    @Override
+    public void attack(Character character) {
+        this.heal(this);
+        character.takeDamage(6);
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        this.setCurrentHealth(damage - this.shield);
+    }
+
+}
