ff2d232a8197de42c2fe9771faabf14afca59b3d
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-15 13:45:43 +0100

    inventory

---
+++ MapInventory/MapInventory.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MapInventory {
+
+    public static int getProductPrice(Map<String, Integer> inventory, String productId) {
+        if (!inventory.containsKey(productId)) {
+            return -1;
+        }
+
+        return inventory.get(productId);
+    }
+
+    public static List<String> getProductIdsByPrice(Map<String, Integer> inventory, int price) {
+        List<String> ids = new ArrayList<>();
+
+        if (inventory == null) {
+            return ids;
+        }
+
+        inventory.forEach((product, p) -> {
+            if (p == price) {
+                ids.add(product);
+            }
+        });
+        return ids;
+    }
+}
