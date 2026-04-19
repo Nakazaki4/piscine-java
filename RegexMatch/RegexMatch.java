8b94bce998bcada09794b34d4a560a1a01d2ac9a
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-19 16:40:14 +0100

    day one friend

---
+++ RegexMatch/RegexMatch.java
@@ -0,0 +1,19 @@
+
+
+public class RegexMatch {
+
+    public static boolean containsOnlyAlpha(String s) {
+        if (s == null) {
+            return false;
+        }
+        return s.matches("[A-Z]");
+    }
+
+    public static boolean startWithLetterAndEndWithNumber(String s) {
+        return s.matches("^[a-zA-Z].*[0-9]$");
+    }
+
+    public static boolean containsAtLeast3SuccessiveA(String s) {
+        return s.matches(".*AAA.*");
+    }
+}
