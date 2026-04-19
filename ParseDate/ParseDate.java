bba52e2c9c2f98870da09677c23740350e140e83
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-19 14:44:08 +0100

    2-in-1

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -9,16 +9,21 @@ import java.util.Locale;
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
+        if (stringDate == null) return null;
         return LocalDateTime.parse(stringDate);
 
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+        if (stringDate == null) return null;
+
         DateTimeFormatter formatter = new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("EEEE d MMMM yyyy").toFormatter(Locale.ENGLISH);
         return LocalDate.parse(stringDate, formatter);
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
+        if (stringDate == null) return null;
+
         String text = stringDate.replace("in the evening", "PM").replace("in the morning", "AM");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh' hours 'a', 'mm' minutes and 'ss' seconds'", Locale.ENGLISH);
         return LocalTime.parse(text, formatter);
