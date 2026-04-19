ebd553d626a1b54123491eb165a9a7db664a59f6
nakazaki4 <nakazakinakazaki22@gmail.com>
2026-04-19 16:42:24 +0100

    day one friend

--- RegexMatch/RegexMatch.java
+++ RegexMatch/RegexMatch.java
@@ -6,14 +6,20 @@ public class RegexMatch {
         if (s == null) {
             return false;
         }
-        return s.matches("[A-Z]");
+        return s.matches("[a-zA-Z]");
     }
 
     public static boolean startWithLetterAndEndWithNumber(String s) {
+        if (s == null) {
+            return false;
+        }
         return s.matches("^[a-zA-Z].*[0-9]$");
     }
 
     public static boolean containsAtLeast3SuccessiveA(String s) {
+        if (s == null) {
+            return false;
+        }
         return s.matches(".*AAA.*");
     }
 }
